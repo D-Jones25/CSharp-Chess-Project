130dbcb5f335daa5876228b90a78d2777dfe7fed
D-Jones25 <109439852+D-Jones25@users.noreply.github.com>
2024-09-10 20:46:03 -0400

    Add files via upload

---
+++ ChessLogic/Pieces/King.cs
@@ -0,0 +1,68 @@
+﻿namespace ChessLogic
+{
+    public class King : Piece
+    {
+        public override PieceType Type => PieceType.King;
+        public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.West,
+            Direction.South,
+            Direction.East,
+
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.SouthWest,
+            Direction.SouthEast
+        };
+
+        public King(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            King copy = new King(Color);
+            copy.HasMoved = true;
+            return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction dir in dirs)
+            {
+                Position to = from + dir;
+
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
+        }
+
+        public override bool CanCaptureOpponentKing(Position from, Board board)
+        {
+            return MovePositions(from, board).Any(to => 
+            {
+                Piece piece = board[to];
+                return piece != null && piece.Type == PieceType.King;
+            });
+        }
+    }
+}
